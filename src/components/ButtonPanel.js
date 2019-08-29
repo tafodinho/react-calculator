78792b69b8680ba239927a9d28e701fc0d63938d
Tafang Joshua <tafodinho@gmail.com>
2019-08-29 10:02:08 +0100

    added functional and controll components

---
+++ src/components/ButtonPanel.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import Button from './Button'
+
+const ButtonPanel = (props) => {
+
+    return (
+        <div class="main">
+            <div class="group-1">
+                <Button name="AC" />
+                <Button name="+/-" />
+                <Button name="%" />
+                <Button name="+" />
+            </div>
+            <div class="group-2">
+                <Button name="7" />
+                <Button name="8" />
+                <Button name="9" />
+                <Button name="X" />
+            </div>
+            <div class="group-3">
+                <Button name="4" />
+                <Button name="5" />
+                <Button name="6" />
+                <Button name="-" />
+            </div>
+            <div class="group-4">
+                <Button name="1" />
+                <Button name="2" />
+                <Button name="3" />
+                <Button name="+" />
+            </div>
+            <div class="group-5">
+                <Button name="0" />
+                <Button name="." />
+                <Button name="=" />
+                <Button name="" />
+            </div>
+        </div>
+    )
+}
+
+export default ButtonPanel
---
+++ src/components/Display.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Display = (props) => {
+    return (
+        <div>
+            {props.result}
+        </div>
+    )
+}
+
+export default Display
