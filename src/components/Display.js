41aa2bdcc535ed1754c1a12fd590e32274ab2939
Tafang Joshua <tafodinho@gmail.com>
2019-08-30 17:38:18 +0100

    adjusted style

--- src/components/Display.js
+++ src/components/Display.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Display = (props) => {
     return (
         <div className="display">
-            {props.result}
+            <p>{props.result}</p>
         </div>
     )
 }
